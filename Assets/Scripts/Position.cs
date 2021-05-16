5b9d9fa84a0070e12796b9178d74551fb6c10452
Aline F. Brito <alinefbrito@gmail.com>
2021-05-16 14:28:51 -0300

    Inclusão dos dados do projeto

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    public Vector3 positionChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = Random.Range(0.0f, 1.0f) / 10.0f;
+        float y = Random.Range(0.0f, 1.0f) / 10.0f;
+        float z = Random.Range(0.0f, 1.0f) / 10.0f;
+        
+        positionChange.Set(x, y, z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += positionChange;
+    }
+}
---
+++ Assets/Scripts/Scale.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+    //instancia a classe Vector que possibilita manipular as dimensões
+    public Vector3 scaleChange;
+    
+   
+    
+
+
+    // Start é chamado antes da atualização do primeiro frame
+    void Start()
+    {
+        float x = Random.Range(0.0f, 1.0f)/10.0f;
+        float y = Random.Range(0.0f, 1.0f) / 10.0f;
+        float z = Random.Range(0.0f, 1.0f) / 10.0f;
+        scaleChange.Set(x, y, z);
+        
+       
+       
+
+    }
+
+    // Update é chamado a cada atualização de frame
+    void Update()
+    {
+        transform.localScale += scaleChange;
+        
+       
+    }
+}
